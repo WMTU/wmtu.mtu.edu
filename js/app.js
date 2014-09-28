4cbe87708f5051591251c6552093f2a4ff937da2
Neil Betham <nbetham@me.com>
2014-09-28 02:12:27 -0400

    Play Pause

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 var WMTU = {};
+WMTU.onStreamPlaying = function(){
+  $('#play-button').hide();
+  $('#pause-button').show();
+};
+
+WMTU.onStreamPaused = function(){
+  $('#play-button').show();
+  $('#pause-button').hide();
+};
 
 WMTU.initStream = function(){
   WMTU.streamObject = soundManager.createSound({
@@ -6,14 +15,37 @@ WMTU.initStream = function(){
     url: 'http://stream.wmtu.mtu.edu:8000/listen',
     autoLoad: true,
     autoPlay: true,
-    volume: 50
+    volume: 80,
+    onplay: WMTU.onStreamPlaying,
+    onresume: WMTU.onStreamPlaying,
+    onpause: WMTU.onStreamPaused
+  });
+
+
+};
+
+WMTU.bindThings = function(){
+  $('#play-button').click(function(){
+    if(WMTU.streamObject.paused){
+      WMTU.streamObject.resume();
+    } else {
+      WMTU.streamObject.play();
+    }
+  });
+  $('#pause-button').click(function(){
+    WMTU.streamObject.pause();
   });
 };
 
-$(document).ready(function(){
+WMTU.setup = function(){
   soundManager.setup({
     url: '/bower_components/soundmanager/swf/',
     preferFlash: false,
     onready: WMTU.initStream
   });
+  WMTU.bindThings();
+};
+
+$(document).ready(function(){
+  WMTU.setup();
 });
