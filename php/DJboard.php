1f28c35168366ce0951e805adef42efa8aa1a828
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2015-10-02 13:51:54 -0400

    Created DJboard.php
    Aiming to build a better board web page because CONCERTO SUCKS

---
+++ php/DJboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WMTU Board</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="description" content="This purpose of this page is to be loaded into and diplayed on the TV inside the WMTU booth">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+</head>
+
+<body>
+It's a page about nothing
+</body>
+
+</html>
