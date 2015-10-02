234883d01d4d0e500b745a17fead1dce28eda71f
Ryan Fenton-Garcia <rjfenton@mtu.edu>
2015-10-02 02:25:05 -0400

    Fixed a URL that directed to my site, to our site.

--- php/date.php
+++ php/date.php
@@ -5,7 +5,7 @@ echo "<?xml version='1.0' encoding='utf-8'?>\n";
 echo "<rss version='2.0'>\n";
 echo "<channel>\n";
 echo "<title>".date("l, F j, Y")."</title>\n";
-echo "<link>http://bustyloli.ch</link>\n";
+echo "<link>http://wmtu.mtu.edu</link>\n";
 echo "<description>The date</description>\n";
 echo "<language>en-us</language>\n";
 echo "<item></item>\n";
