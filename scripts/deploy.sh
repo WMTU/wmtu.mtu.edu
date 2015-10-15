7fd39c08aa62377502e3c2d245eebd572f3a740d
Neil Betham <nbetham@me.com>
2015-10-14 23:17:51 -0700

    Try using rvm to install newer ruby

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'Installing Newer Ruby'
+rvm install 2.2.1
+rvm use --default 2.2.1
+
 echo 'Installing SCP Gem'
 gem install net-scp
-
 
 RUBY_VERSION=`ruby -v`
 echo "Ruby Version $RUBY_VERSION"
