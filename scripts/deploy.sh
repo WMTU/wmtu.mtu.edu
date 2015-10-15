62b06062d1c44194a611479ceb89116b1f78ec1e
Neil Betham <nbetham@me.com>
2015-10-14 23:04:46 -0700

    Why is deploy environment soooo different

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,9 @@
 echo 'Installing SCP Gem'
 gem install net-scp
 
+
+RUBY_VERSION=`ruby -v`
+echo 'Ruby Version $RUBY_VERSION'
+
 echo 'Copying Built Site to Server'
 ruby scripts/scp.rb $USERNAME $PASSWORD wmtu.mtu.edu _site /var/www/
