61593d0a2a8fbb7b47a825b080e282587a4bb6d7
Neil Betham <nbetham@me.com>
2015-10-14 23:01:51 -0700

    welp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Installing SCP Gem'
-travis_retry gem install net-scp
+gem install net-scp
 
 echo 'Copying Built Site to Server'
 ruby scripts/scp.rb $USERNAME $PASSWORD wmtu.mtu.edu _site /var/www/
