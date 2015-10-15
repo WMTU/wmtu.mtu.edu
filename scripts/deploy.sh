9589294fee334fe07349879147305a28db29b7c5
Neil Betham <nbetham@me.com>
2015-10-14 23:43:00 -0700

    Move back to after success

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-echo 'Using Newer Ruby'
-rvm use 2.2.1 --install --binary --fuzzy
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo "Checking Git Branch $DEPLOY_BRANCH"
 
-echo 'Installing SCP Gem'
-gem install net-scp
-
-RUBY_VERSION=`ruby -v`
-echo "Ruby Version $RUBY_VERSION"
+if [ "$CURRENT_BRANCH" != "$DEPLOY_BRANCH" ]; then
+  echo "Not On $DEPLOY_BRANCH, Not Deploying"
+  exit 0;
+fi
 
 echo 'Copying Built Site to Server'
 ruby scripts/scp.rb $USERNAME $PASSWORD wmtu.mtu.edu _site /var/www/
