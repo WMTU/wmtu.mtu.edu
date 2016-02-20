907696d2e1899414d90700f9865bc4f0fda47567
Joel Yauk <jbyauk@gmail.com>
2016-02-19 23:34:54 -0500

    Update playlist script to use PDO

--- php/playlist.php
+++ php/playlist.php
@@ -2,42 +2,33 @@
   // Create connection
   $configs = include('config.php');
 
-  $con = mysqli_connect($configs['hostname'], $configs['username'], $configs['password'], $configs['database']);
-  $con->set_charset("utf8");
+  $con = new PDO( sprintf( 'mysql:host=%s;dbname=%s;charset=utf8',
+      $configs['hostname'], $configs['database'] ),
+      $configs['username'], $configs['password'] );
+  $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  $con->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 
-  // Check connection
-  if (mysqli_connect_errno()){
-    echo "{\"error\":\"".mysqli_connect_error()."\"}";
-    die();
-  }
-
-  if(!$_POST[day] | !$_POST[month] | !$_POST[year]){
+  if ( !$_POST['day'] | !$_POST['month'] | !$_POST['year'] ) {
     $sql = "SELECT * FROM djlogs WHERE ts > CURDATE() ORDER BY id DESC";
-  }else{
-    $day = filter_input(INPUT_POST, 'day', FILTER_SANITIZE_NUMBER_INT);
-    $month = filter_input(INPUT_POST, 'month', FILTER_SANITIZE_NUMBER_INT);
-    $year = filter_input(INPUT_POST, 'year', FILTER_SANITIZE_NUMBER_INT);
+    $stmt = $con->prepare( $sql );
+  } else {
+    $day = filter_input( INPUT_POST, 'day', FILTER_SANITIZE_NUMBER_INT );
+    $month = filter_input( INPUT_POST, 'month', FILTER_SANITIZE_NUMBER_INT );
+    $year = filter_input( INPUT_POST, 'year', FILTER_SANITIZE_NUMBER_INT );
+    $date = sprintf( '%s/%s/%s', $month, $day, $year );
+    $day_start = strtotime( $date . ' 00:00:00' );
+    $day_end = strtotime( $date . ' 23:59:59' );
 
-    $sql = "SELECT * FROM djlogs WHERE ts BETWEEN TIMESTAMP('".$year."-".$month."-".$day."') AND TIMESTAMP('".$year."-".$month."-".$day." 23:59:59') ORDER BY id DESC";
+    $sql = "SELECT * FROM djlogs WHERE ts BETWEEN :start AND :end ORDER BY id DESC";
+    $stmt = $con->prepare( $sql );
+    $stmt->bindValue( ':start', $day_start );
+    $stmt->bindValue( ':end', $day_end );
   }
 
+  $stmt->execute();
+  $rows = $stmt->fetchAll( PDO::FETCH_ASSOC );
 
+  echo json_encode( $rows );
 
-  // Execute query
-  if ($result = mysqli_query($con,$sql))
-  {
-    $rows = array();
-    //success
-    while($assoc = mysqli_fetch_assoc($result)){
-      array_push($rows, $assoc);
-    }
-
-    echo json_encode($rows);
-  }
-  else
-  {
-    echo "Error parsing data: " . mysqli_error($con);
-  }
-
-  mysqli_close($con);
+  $con = null;
 ?>
